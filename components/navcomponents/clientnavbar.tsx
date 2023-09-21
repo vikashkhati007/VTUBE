3ca03db6e0af2774dd2d7db328fec4ef7fcf04ba
vikashkhati007 <vikashkhati007@gmail.com>
2023-09-21 20:26:35 +0530

    vtube start

--- components/navcomponents/clientnavbar.tsx
+++ components/navcomponents/clientnavbar.tsx
@@ -8,9 +8,9 @@ import HeadItemsScroll from "./headitemsscroll";
 const ClientNavbar = () => {
   return (
     <div className="flex w-[94vw] flex-col h-screen absolute right-0 top-0">
-      <div className="clientcontainer flex flex-row justify-around items-center w-full h-16 bg-[#0F0F0F]">
+      <div className="clientcontainer flex flex-row justify-around items-center w-full h-16 bg-[#0F0F0F] border-b border-white border-opacity-10">
         <div className="w-[6rem]">
-          <YoutubeLogo />
+          <h1 className="font-bold text-white text-3xl"><span className="logo">V</span>TUBE</h1>
         </div>
         <div>
           <Input />
--- components/navcomponents/navbaritems.tsx
+++ components/navcomponents/navbaritems.tsx
@@ -8,7 +8,7 @@ import ItemsMenuSVG from "./svg/itemsmenu";
 const NavbarItems = () => {
   return (
     <>
-      <div className="absolute top-0 left-0 pt-3 flex flex-col justify-start space-y-5 items-center w-39 h-screen bg-[#0F0F0F] text-white">
+      <div className="absolute top-0 left-0 pt-3 flex flex-col justify-start space-y-10 items-center w-[6%] h-screen bg-[#0F0F0F] text-white border-r border-white border-opacity-10">
       <div className="homesvgcontainer w-15 flex flex-col items-center p-2 rounded-md cursor-pointer hover:bg-white hover:bg-opacity-10">
           <div className="homesvg w-7 ">
             <ItemsMenuSVG/>
@@ -18,25 +18,21 @@ const NavbarItems = () => {
           <div className="homesvg w-7 ">
             <HomeIconSVG />
           </div>
-          <p className="text-xs">Home</p>
         </div>
         <div className="homesvgcontainer w-15 flex flex-col items-center p-2 cursor-pointer hover:bg-white hover:bg-opacity-10 rounded-md">
           <div className="homesvg w-7 ">
             <ShortsIconSVG />
           </div>
-          <p className="text-xs">Shorts</p>
         </div>
         <div className="homesvgcontainer w-15 flex flex-col items-center p-2 cursor-pointer hover:bg-white hover:bg-opacity-10 rounded-md">
-          <div className="homesvg w-7 ">
+          <div className="homesvg w-7">
             <SubscriptionIcon />
           </div>
-          <p className="text-[10px]">Subscriptions</p>
         </div>
         <div className="homesvgcontainer w-15 flex flex-col items-center p-2 cursor-pointer hover:bg-white hover:bg-opacity-10 rounded-md">
-          <div className="homesvg w-7 ">
+          <div className="homesvg w-7">
             <LibraryIconSVG />
           </div>
-          <p className="text-xs">Library</p>
         </div>
       </div>
     </>
