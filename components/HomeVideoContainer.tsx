81e1f83f54fd6a637597bffbca0df6eb1925412c
vikashkhati007 <vikashkhati007@gmail.com>
2023-09-29 09:15:24 +0530

    userdata

--- components/HomeVideoContainer.tsx
+++ components/HomeVideoContainer.tsx
@@ -1,50 +1,53 @@
-"use client"
+"use client";
 import Video from "./Video";
 import Link from "next/link";
 import youtube from "../youtubedata/gethomedata";
-import { useState } from "react";
-import UserProfileData from "@/youtubedata/userprofiledata";
-
+import getUserProfileData from "../youtubedata/userprofiledata";
 const HomeVideoContainer = async () => {
   const youtubehomedata = await youtube();
 
   return (
     <div className="videocontainer flex flex-wrap items-center justify-around text-white border-b-2 border-white border-opacity-20 ">
-    {youtubehomedata.items.map( async (d:any, index:any) => {
-      const data = await UserProfileData(d.snippet.channelId);
-  // Safely parse the views count
-  const views = parseInt(String(d.snippet.categoryId || '0'), 10);
+      {youtubehomedata.items.map(async (d: any, index: any) => {
+        const channelIds = youtubehomedata.items.map(
+          (d: any) => d.snippet.channelId
+        );
+        const userdata = await getUserProfileData(channelIds);
+        userdata?.map((u:any) => {
 
-  // Format the videoviews prop with abbreviations
-  let formattedViews;
+          console.log();
+          // Safely parse the views count
+          const views = parseInt(String(d.snippet.categoryId || "0"), 10);
 
-  if (views >= 1e9) {
-    formattedViews = (views / 1e9).toFixed(1) + 'B';
-  } else if (views >= 1e6) {
-    formattedViews = (views / 1e6).toFixed(1) + 'M';
-  } else if (views >= 1e3) {
-    formattedViews = (views / 1e3).toFixed(1) + 'K';
-  } else {
-    formattedViews = views.toString();
-  }
+          // Format the videoviews prop with abbreviations
+          let formattedViews;
 
-  return (
-    <>
-    <Link key={d.id} href={`/watch/${d.id}`}>
-    <Video
-      thumbnail={`${d.snippet.thumbnails.standard.url}`}
-      videotitle={`${d.snippet.title.slice(0, 29) + "..."}`}
-      channelavtar={`${""}`}
-      channelname={`${""}`}
-      videoviews={formattedViews || ""} // Use the formatted views prop
-      videouploaddate={`${d.snippet.publishedAt}`}
-    />
-    </Link>
-    </>
-  );
-})}
+          if (views >= 1e9) {
+            formattedViews = (views / 1e9).toFixed(1) + "B";
+          } else if (views >= 1e6) {
+            formattedViews = (views / 1e6).toFixed(1) + "M";
+          } else if (views >= 1e3) {
+            formattedViews = (views / 1e3).toFixed(1) + "K";
+          } else {
+            formattedViews = views.toString();
+          }
 
-
+          return (
+            <>
+              <Link key={d.id} href={`/watch/${d.id}`}>
+                <Video
+                  thumbnail={`${d.snippet.thumbnails.standard.url}`}
+                  videotitle={`${d.snippet.title.slice(0, 29) + "..."}`}
+                  channelavtar={`${""}`}
+                  channelname={`${u.items[0].snippet.title}`}
+                  videoviews={formattedViews || ""} // Use the formatted views prop
+                  videouploaddate={`${d.snippet.publishedAt}`}
+                />
+              </Link>
+            </>
+          );
+        });
+      })}
     </div>
   );
 };
--- youtubedata/userprofiledata.ts
+++ youtubedata/userprofiledata.ts
@@ -1,17 +1,22 @@
-
-const UserProfileData = async ( channelId : any) => {
-//channelid get
-
-    try {
-        const d = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&id=${channelId[0]}&key=${process.env.API_KEY}`);
-        const data = await d.json();
-        return data;
+const getUserProfileData = async (channelIds:any) => {
+  try {
+    const promises = channelIds.map(async (channelId:any) => {
+      const response = await fetch(`https://www.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails&id=${channelId}&key=${process.env.API_KEY}`);
+      
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
       }
-    catch (error) {
-      console.error(error);
-      return []; // Return an empty array in case of an error
-    }
-  };
-  
-  export default UserProfileData;
-  
+
+      const data = await response.json();
+      return data;
+    });
+
+    const userData = await Promise.all(promises);
+    return userData;
+  } catch (error) {
+    console.error(error);
+    return null; // Return null in case of an error
+  }
+};
+
+export default getUserProfileData;
